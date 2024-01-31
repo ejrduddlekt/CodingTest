2c55adf0fc86730f1e355456689b5061b9f345e2
DESKTOP-PM3TPIM\admin <ejrdud1256@gmail.com>
2024-02-01 02:55:56 +0900

    Lv2_카펫문제(완전탐색)

---
+++ CodingTest/프로그래머스/알고리즘/완전탐색/Level2_카펫.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 프로그래머스.알고리즘.완전탐색
+{
+    internal class Level2_카펫
+    {
+        public int[] solution(int brown, int yellow)
+        {
+            int[] answer = new int[] { };
+
+            int addValue = brown + yellow;
+            int sqrtValue = (int)Math.Sqrt(addValue);
+
+            for (int i = 2; i <= sqrtValue; i++)
+            {
+                if ((addValue % i) == 0)
+                {
+                    int horizontal = i - 2;
+                    int vertical = (addValue / i) - 2;
+
+                    if(horizontal * vertical == yellow)
+                    {
+                        answer = horizontal > vertical ? new int[] { i, addValue / i } : new int[] { addValue / i, i };
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
