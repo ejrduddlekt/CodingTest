0b9d2551cf30c6b715a7b4c80ff34076ea3b2299
DESKTOP-PM3TPIM\admin <ejrdud1256@gmail.com>
2024-02-04 18:13:22 +0900

    코딩인터뷰 완전분석 / 배열과 문자열 / 중복된 문자열 찾기

---
+++ CodingTest/프로그래머스/코딩인터뷰_완전분석/1_배열과문자열/1_중복된_문자열_찾기.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 프로그래머스.코딩인터뷰_완전분석._1_배열과문자열
+{
+    internal class _1_중복된_문자열_찾기
+    {
+        public bool solution_1(string testCase)
+        {
+            for (int i = 0; i < testCase.Length - 1; i++)
+                for (int j = i + 1; j < testCase.Length; j++)
+                    if (testCase[i] == testCase[j])
+                        return false;
+            return true;
+        }
+
+        public bool solution_2(string testCase)
+        {
+            Dictionary<int, bool> dicString = new Dictionary<int, bool>();
+
+            for (int i = 0; i < testCase.Length; i++)
+            {
+                if (dicString.ContainsKey(testCase[i]))
+                {
+                    return false;
+                }
+                dicString.Add(testCase[i], true);
+            }
+            return true;
+        }
+    }
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        _1_중복된_문자열_찾기 solution = new _1_중복된_문자열_찾기();
+
+    //        Console.WriteLine($"Result: {solution.solution_2("")}");
+    //        Console.WriteLine($"Result: {solution.solution_2("abcd")}");
+    //        Console.WriteLine($"Result: {solution.solution_2("abccd")}");
+    //        Console.WriteLine($"Result: {solution.solution_2("bhjjb")}");
+    //        Console.WriteLine($"Result: {solution.solution_2("mdjq")}");
+    //        Console.WriteLine($"Result: {solution.solution_2("geez")}");
+    //    }
+    //}
+}
